326d80d150565e48875d177a12b92fe1c3be3275
LinoRino <omurakenshin@gmail.com>
2022-11-21 11:14:53 +0900

    Fist comit

---
+++ assets/components/Dialog/Dialog.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useState } from "react";
+import "./Dialog.module.css";
+
+export function Dialog(prop: any): JSX.Element {
+  return <div>write here to create a component</div>;
+}
