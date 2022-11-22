569ab9283d463e2b8be12c4b384ad0a7d223f200
LinoRino <omurakenshin@gmail.com>
2022-11-22 16:37:08 +0900

    Changed: sidebar design

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import AppBar from "./assets/components/AppBar/AppBar";
 import Sbar_item from "./assets/components/Sbar_item/Sbar_item";
 
 function App() {
-  const items: { href: string; title: string }[] = [
-    { href: "/", title: "home" },
-    { href: "/", title: "profile" },
+  const items: { href: string; title: string, icon: string }[] = [
+    { href: "/", title: "home",icon:"🏠" },
+    { href: "/", title: "profile", icon:"🕺" },
+    { href: "/", title: "setting", icon:"⚙️" },
   ];
   return (
     <>
       <AppBar>jj</AppBar>
       <div className="fle">
-        <div className="flex flex-col p-3 mx-2 my-3 w-60">
+        <div className="flex flex-col p-3 mx-2 my-3 w-64">
           {items.map((item, idx) => (
-            <Sbar_item href={item.href} title={item.title} />
+            <Sbar_item href={item.href} title={item.title} icon={item.icon}/>
           ))}
         </div>
         <main></main>
--- src/assets/components/Sbar_item/Sbar_item.tsx
+++ src/assets/components/Sbar_item/Sbar_item.tsx
@@ -3,10 +3,11 @@ import React from "react";
 export default function Sbar_item(props: any): JSX.Element {
   return (
     <a
-      className="AdvenccreCmp-Sbar_item active:bg-slate-100 sticky top-0 left-0 w-full py-2 px-3 rounded-md"
+      className="AdvenccreCmp-Sbar_item active:bg-lime-200 items-center sticky top-0 left-0 flex py-2 px-3 rounded-md"
       href={props.href}
     >
-      <span className=" font-semibold">{props.title}</span>
+      <span className="mr-4 text-2xl">{props.icon}</span>
+      <span className="font-semibold text-xl">{props.title}</span>
     </a>
   );
 }
