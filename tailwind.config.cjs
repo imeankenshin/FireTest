64386af4237aeb7007a4a4f3a126c63d923d5532
LinoRino <omurakenshin@gmail.com>
2022-12-06 11:37:22 +0900

    Add: Tailwind plugins

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -7,7 +9,15 @@ module.exports = {
         0.5: "0.5px",
         1.5: "1.5px",
       },
+      aspectRatio: {
+        "rect": "2 / 1",
+        "4/3": "4 / 3"
+      },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/container-queries'),
+    plugin(function({ addVariant }) {
+      addVariant('child', '&>*')
+    })
+  ],
 };
