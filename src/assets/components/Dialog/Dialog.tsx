b2d5c659de67efa843d9bb38064913fb0b24dac6
LinoRino <omurakenshin@gmail.com>
2023-01-16 09:38:36 +0900

    Add: Dialog component

--- src/assets/components/Dialog/Dialog.tsx
+++ src/assets/components/Dialog/Dialog.tsx
@@ -1,7 +1,55 @@
-import React from "react";
-import { useState } from "react";
-import "./Dialog.module.css";
+import { useEffect, useRef, useState } from "react";
 
-export function Dialog(prop: any): JSX.Element {
-  return <div>write here to create a component</div>;
+type Adv_Dialog = {
+  children?: JSX.Element | null;
+  isOpen?: boolean | (() => boolean);
+};
+export function Dialog(prop: Adv_Dialog): JSX.Element {
+  const [open, setOpen] = useState<boolean>(true);
+  let tabIndex = 0;
+  document.addEventListener("keydown", function (e) {
+    if (e.key == "Escape") {
+      setOpen(false);
+    }
+  });
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+      tabIndex = 0;
+    } else {
+      document.body.style.overflow = "auto";
+      tabIndex = -1;
+    }
+  }, [open]);
+  return (
+    <dialog
+      tabIndex={tabIndex}
+      className={`fixed top-0 left-0 z-50 grid h-screen w-screen place-items-center bg-gray-700 p-4 ${
+        open ? "bg-opacity-30" : "-z-10 select-none bg-opacity-0"
+      } transition-[transform_opacity] duration-200`}
+    >
+      <div
+        className={`flex rounded-lg bg-white p-6 transition-[transform_opacity] ${
+          !open ? "scale-90 opacity-0" : "scale-100 opacity-100 duration-200"
+        } cursor-default`}
+      >
+        <div>{prop.children}</div>
+        <button
+          title="close a dialog"
+          onClick={() => setOpen(!open)}
+          type="button"
+          className="ml-2 -mt-1 -mr-1 grid h-9 w-9 place-items-center rounded-lg
+                bg-black bg-opacity-30 p-1.5 text-center text-xl text-white
+                active:scale-95 active:bg-opacity-40"
+        >
+          <span
+            className="-translate-y-[1.5px] select-none font-medium"
+            translate="no"
+          >
+            􀆄
+          </span>
+        </button>
+      </div>
+    </dialog>
+  );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,6 +4,7 @@ import { currentUser } from "../../firebase/auth";
 import { User } from "firebase/auth";
 import Profile from "./components/Profile";
 import WelcomeMenu from "./components/WelcomeMenu";
+import { Dialog } from "../../assets/components/Dialog/Dialog";
 
 const user: User | undefined = await currentUser();
 
@@ -16,6 +17,25 @@ export function Home() {
         <Profile />
         {/* Welcome menu */}
         <WelcomeMenu login={user != undefined} />
+        <Dialog isOpen={true}>
+          <div className="w-screen max-w-xl">
+            <h1 className="mb-4 border-b-2 border-gray-300 pb-2 text-3xl font-semibold">
+              Feature of v3.7.15
+            </h1>
+            <p>
+              Hi, lazy nerds!👋 We are so exited about today's awesome update!
+            </p>
+            <h2 className="my-3 text-2xl font-semibold">What's new?</h2>
+            yoyou've got a friedn of me.
+            <ul className="list-disc decoration-clone p-2 pl-6">
+              <li>first</li>
+              <li>second</li>
+              <li>third</li>
+              <li>forth</li>
+            </ul>
+            <button onClick={() => alert("null")}>this</button>
+          </div>
+        </Dialog>
       </div>
     </main>
   );
