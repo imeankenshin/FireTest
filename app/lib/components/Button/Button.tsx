7ce30972919676f6fc2313f3f66b4ac06a1f43f9
LinoRino <omurakenshin@gmail.com>
2023-01-31 10:17:55 +0900

    Fix: style

--- app/lib/components/Button/Button.tsx
+++ app/lib/components/Button/Button.tsx
@@ -4,12 +4,33 @@ type V2ButtonComponent = {
 	onClick?: React.MouseEventHandler<HTMLButtonElement>;
 	title?: string;
 	id?: string;
-	type?: 'button' | 'submit' | 'reset';
+	type: 'button' | 'submit' | 'reset';
+	size: 'sm' | 'md' | 'lg';
 	name?: string;
 	children?: React.ReactNode;
+	fullWidth?: boolean;
 };
 
-function Btn(props: V2ButtonComponent) {
+function Btn(
+	props: V2ButtonComponent = {
+		size: 'md',
+		type: 'button'
+	}
+) {
+	// Btn sizes
+	const sm = 'text-sm ';
+	const md = 'px-3';
+	const lg = '';
+	const sizeSelector = () => {
+		switch (props.size) {
+			case 'lg':
+				return lg;
+			case 'md':
+				return md;
+			case 'sm':
+				return sm;
+		}
+	};
 	return (
 		<button
 			onClick={props.onClick}
@@ -17,7 +38,9 @@ function Btn(props: V2ButtonComponent) {
 			id={props.id}
 			type={props.type}
 			name={props.name}
-			className="mx-2 rounded-md bg-violet-500 py-2.5 px-3 active:scale-95"
+			className={`${sizeSelector()} ${
+				props.fullWidth && 'w-full'
+			} mx-2 rounded-md bg-violet-500 p-2 font-medium active:scale-95`}
 		>
 			{props.children}
 		</button>
--- app/lib/components/TxtInput/TxtInput.tsx
+++ app/lib/components/TxtInput/TxtInput.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-type TxtInputComponent = {
+type V2TxtInputComponent = {
 	// strings
 	name: string;
 	id?: string;
 	title?: string;
 	property?: string;
+	placeholder?: string;
+	description?: string;
 	label?: string | JSX.Element;
-	type: 'text' | 'email' | 'password' | 'tel';
+	type: React.HTMLInputTypeAttribute;
 	autoComplete?: 'on' | 'off';
 	autoSave?: 'on' | 'off';
 	// booleans
@@ -22,31 +24,29 @@ type TxtInputComponent = {
 	// functions
 	refs?: React.LegacyRef<HTMLInputElement>;
 	onChange?: React.ChangeEventHandler<HTMLInputElement>;
+	onLoad?: React.ReactEventHandler<HTMLInputElement>;
 	onFocus?: React.FocusEventHandler<HTMLInputElement>;
 	onClick?: React.MouseEventHandler<HTMLInputElement>;
+	onMouseEnter?: React.MouseEventHandler<HTMLInputElement>;
+	onMouseLeave?: React.MouseEventHandler<HTMLInputElement>;
 };
 
-export function TxtInput(props: TxtInputComponent) {
+export function TxtInput(props: V2TxtInputComponent) {
 	const [show, setShow] = useState(false);
 	return (
 		<label className={`${props.disabled && 'cursor-not-allowed opacity-80'} flex flex-col-reverse`}>
-			<div className="flex w-full items-center justify-end">
+			<div className="flex items-center justify-end">
 				<input
 					onChange={(e) => {
 						if (props.onChange) {
 							props.onChange(e);
 						}
 					}}
-					onFocus={(e) => {
-						if (props.onFocus) {
-							props.onFocus(e);
-						}
-					}}
-					onClick={(e) => {
-						if (props.onClick) {
-							props.onClick(e);
-						}
-					}}
+					onFocus={props.onFocus}
+					onMouseEnter={props.onMouseEnter}
+					onMouseLeave={props.onMouseLeave}
+					onClick={props.onClick}
+					onLoad={props.onLoad}
 					property={props.property}
 					autoComplete={props.autoComplete}
 					autoSave={props.autoSave}
@@ -58,25 +58,26 @@ export function TxtInput(props: TxtInputComponent) {
 					disabled={props.disabled}
 					required={props.required}
 					readOnly={props.readOnly}
+					placeholder={props.placeholder}
 					type={show ? 'text' : props.type}
 					maxLength={props.maxLength}
 					minLength={props.minLength}
-					className={`w-full rounded-md bg-gray-400/30 p-2 ${
+					className={`w-full rounded-md bg-gray-400/30 py-2 px-2.5 ${
 						props.type == 'password' && 'pr-10'
-					} outline-none`}
+					} outline-none ring-blue-400 focus-within:ring-2`}
 				/>
 				{props.type == 'password' && (
 					<button
 						type="button"
 						title={`${show ? 'Hide' : 'Show'} password`}
 						onClick={() => setShow(!show)}
-						className="absolute mr-2 h-6 w-6 rounded text-xs outline-none hover:bg-gray-400/60 focus:bg-gray-400/30 active:scale-90"
+						className="absolute mr-2.5 h-6 w-6 rounded text-xs outline-none hover:bg-gray-400/60 focus:bg-gray-400/30 active:scale-90"
 					>
 						{show ? '􀋰' : '􀋮'}
 					</button>
 				)}
 			</div>
-			<span className="text-lg">
+			<span className="my-1.5 text-lg font-medium">
 				{props.label}
 				{props.required && <span className="ml-0.5 text-red-500">*</span>}
 			</span>
