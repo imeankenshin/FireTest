c61f7be20e7ebc1aa0bcb5cf5de62f0030942ab9
LinoRino <omurakenshin@gmail.com>
2023-02-01 11:40:02 +0900

    Add: new variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./app/**/*.{html,ts,tsx,js,jsx}'],
 	theme: {
 		extend: {
-			height: { 'screen-=nav': 'calc(100vh　-　4rem)' },
+			height: { 'screen-=nav': 'calc(100vh　-　4rem)' }
 		}
 	},
-	plugins: []
+	plugins: [
+		plugin(function ({ addVariant }) {
+			addVariant('scroll', '&::-webkit-scrollbar');
+			addVariant('scroll-tb', '&::-webkit-scrollbar-thumb');
+			addVariant('scroll-tb-active', '&::-webkit-scrollbar-thumb:active');
+			addVariant('child', '&>*');
+			addVariant('childAll', '&_*');
+		})
+	]
 };
